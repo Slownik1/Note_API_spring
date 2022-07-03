7b95a7fd32f26f65a3f83442b1df75b126b4bfec
dawid białek <dawid1bialek@gmail.com>
2022-07-03 22:37:42 +0200

    Overload method readAll with pagable argument

--- src/main/java/com/example/note/Contrloler/NoteControler.java
+++ src/main/java/com/example/note/Contrloler/NoteControler.java
@@ -4,6 +4,8 @@ import com.example.note.Model.Note;
 import com.example.note.Model.NoteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,15 +18,21 @@ import java.util.List;
 public class NoteControler {
 
     private static final Logger logger = LoggerFactory.getLogger(NoteControler.class);
-    private final NoteRepository repository;
+    private NoteRepository repository = null;
 
     public NoteControler(NoteRepository repository) {
         this.repository = repository;
     }
-    @GetMapping(path="/notes")
+    @GetMapping(path="/notes", params = {"!sort", "!page","!size"})
     ResponseEntity<?> readAllNote(){
         logger.warn("Test loggera");
         return ResponseEntity.ok(repository.findAll());
     };
+
+    @GetMapping(path ="/notes")
+    ResponseEntity<?> readAllNote(Pageable page){
+        logger.warn("Test loggera 2");
+        return ResponseEntity.ok(repository.findAll(page));
+    }
 
 }
